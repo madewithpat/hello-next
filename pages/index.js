fb853d5975777030382f19a5c7f3bade117b0bec
Patrick Outler <outler.patrick@gmail.com>
2019-01-26 10:27:42 -0500

    modified PostLink to use route masking.
    Need to add server side support for clean URLS

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
 import Layout from "../layouts";
 
-const PostLink = ({ title }) => (
+const PostLink = ({ id, title }) => (
    <li>
-      <Link href={`/post?title=${title}`}>
+      <Link as={`/p/${id}`} href={`/post?title=${title}`}>
          <a>{title}</a>
       </Link>
    </li>
@@ -13,9 +13,9 @@ const Index = () => (
    <Layout>
       <h1>A Sweet Blog</h1>
       <ul>
-         <PostLink title="Hello Next.js" />
-         <PostLink title="Learn Next.js" />
-         <PostLink title="Deploying with Zeit" />
+         <PostLink id="hello-nextjs" title="Hello Next.js" />
+         <PostLink id="learn-nextjs" title="Learn Next.js" />
+         <PostLink id="deploying-with-zeit" title="Deploying with Zeit" />
       </ul>
    </Layout>
 );
